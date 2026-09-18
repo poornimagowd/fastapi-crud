82428248f5d21759614e9b33cc60f712a1e968dd
unknown <poornimalatha28@gmail.com>
2026-09-18 09:59:43 +0530

    chore: get and post methods

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    text:str = None
+    is_done: bool = False
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+Items = []
+
+@app.post('/items')
+def addItems(item: Item):
+    Items.append(item)
+    return Items
+
+
+@app.get('/items/{itemid}', response_model=Item)
+def getItemsById(itemid: int)->Item:
+    if itemid < len(Items): 
+        return Items[itemid]
+    else:
+        raise HTTPException(status_code=404, detail= f"Item not found")
